1cf9fce3075703fa66fc21422b28de9aa37b829d
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2024-07-17 09:21:43 +0200

    Game

---
+++ 10-Running Game/main.js
@@ -0,0 +1,11 @@
+/** @type {HTMLCanvasElement} */
+
+//window.addEventListener('load', e => {
+    window.addEventListener('DOMContentLoaded', e => {
+    const loading = document.getElementById('loading');
+
+    loading.style.display = 'none';
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+ 
+});
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Javascript games
 
 See https://www.youtube.com/watch?v=GFO_txvwK_c 
-Till: 6:59:00
+Till: 7:08:00
 
 See also [Angel Policarpo](https://github.com/AngelPolicarpo/FCC-GameDev-Course)
 
