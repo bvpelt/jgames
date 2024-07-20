585fa53712deefd3a7d21f2bc0b321b9a687df71
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2024-07-20 11:26:52 +0200

    Game

--- 10-Running Game/main.js
+++ 10-Running Game/main.js
@@ -1,7 +1,7 @@
 import { Player } from './player.js';
 import { InputHandler } from './input.js';
 import { Background } from './background.js';
-import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from './enemies.js';
+import { FyingEnemy, ClimbingEnemy, GroundEnemy } from './enemies.js';
 
 window.addEventListener('load', e => {
     const loading = document.getElementById('loading');
@@ -35,19 +35,30 @@ window.addEventListener('load', e => {
             if (this.enemyTimer > this.enemyInterval) {
                 this.addEnemy();
                 this.enemyTimer = 0;
-            }
+            } else {
+                this.enemyTimer += deltaTime;
+            }    
             this.enemies.forEach(enemy => {
                 enemy.update(deltaTime);
-            });
+                if (enemy.markedForDeletion) {
+                    this.enemies.splice(this.enemies.indexOf(enemy), 1);
+                }
+            });   
         }
 
         draw(context) {
             this.background.draw(context);
             this.player.draw(context);
+            this.enemies.forEach(enemy => {
+                enemy.draw(context);
+            });
         }
 
         addEnemy() {
-            this.enemies.push(new FlyingEnemy(this));
+            if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+            else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+            this.enemies.push(new FyingEnemy(this));
+            console.log(this.enemies);
         }
     }
 
